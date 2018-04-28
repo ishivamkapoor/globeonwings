f5f23f5befed5c9210e07f33c3b3318de155edd2
baljeetSingh21 <baljeet.singh@venturepact.com>
2018-04-28 16:45:26 +0530

    27042018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,6 +16,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import {AppRouter} from './app.router';
+import {DateAdapter, MatIconModule, MatNativeDateModule} from '@angular/material';
 
 
 @NgModule({
@@ -22,7 +30,14 @@ import {AppRouter} from './app.router';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatInputModule,
+    MatNativeDateModule,
     MatAutocompleteModule,
+    MatDatepickerModule,
+    MatCheckboxModule,
+    MatRadioModule,
     AppRouter
   ],
   providers: [],
